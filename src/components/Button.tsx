176bb5e752406ac38d59ef742050bcf1fdfaed96
Dale B <daleban.dev@gmail.com>
2024-02-14 22:45:24 +0800

    style: main page design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ export default function Button({
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className={`transition-colors px-6 py-3 rounded font-medium text-sm sm:text-base lg:text-lg ${className}`}
+      className={`transition-colors px-6 py-2 rounded border border-white/40 font-normal text-sm sm:text-base ${className}`}
       {...rest}
     >
       {rest.children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import localFont from "next/font/local";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { toast } from "sonner";
+import Image from "next/image";
 
 import { db } from "@/config/firebase";
 import Modal from "@/components/Modal";
@@ -73,23 +74,39 @@ export default function Home() {
         onClose={() => null}
       />
 
-      <div className="glassmorph border z-30 relative px-7 mx-auto inline-block">
+      <Image
+        height={2552}
+        width={1436}
+        src={"/images/main-bg.png"}
+        alt="Background image"
+        className="absolute top-0 left-0 z-0 object-cover w-full h-full"
+      />
+
+      <div className="glassmorph bottom-12 border-2 rounded-[3rem] border-white/25 max-w-screen-sm z-30 relative p-12 mx-auto inline-block">
+        <div className="text-white space-y-2 text-center font-bold border-b border-white/20 pb-5">
+          <h1 className="text-5xl">Hacking AI</h1>
+          <p className=" text-2xl">Chatbots for Daily Use</p>
+        </div>
+
         <form
           onSubmit={handleLocate}
           className="flex flex-col gap-2 py-5  w-full sm:max-w-[400px] max-w-[350px] text-black"
         >
+          <p className="text-white font-normal text-center">
+            Kindly fill in to claim certificate
+          </p>
           <Input
             required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             type="email"
             placeholder="Enter your email"
-            className="rounded-full pl-5 py-3"
+            className="rounded-full pl-5 py-2 border-gray-700/75 border-[1.5px]"
           />
           <Button
             type="submit"
             disabled={loading}
-            className={`bg-green-500 rounded-full text-white ${googleMedium.className}`}
+            className={`rounded-full text-white ${googleMedium.className}`}
           >
             Claim Certificate
           </Button>
